dfc9f7b322328ca2388f6cda400325f97ce6adfd
MarkMwago2 <markmwago@gmail.com>
2019-06-24 15:31:40 +0300

    add a success page for creating heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,5 +21,16 @@ public class App {
             return new ModelAndView(model, "addnewhero.hbs");
         }, new HandlebarsTemplateEngine());
 
+        post ("/heroes",(request,response)->{
+            Map <String,Object> model = new HashMap<String,Object>();
+            ArrayList<Hero> heroes=request.session().attribute("heroes");
+            String name =request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            String specialPowers =request.queryParams("specialPowers");
+            String weakness =request.queryParams("weakness");
+            Hero newHero = new Hero(name,age,specialPowers,weakness);
+            model.put("templates","templates/success.hbs");
+            return new ModelAndView(model,"heroes.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
