fa88a73a02cfee704ad67ea1060b4f8c963a350f
MarkMwago2 <markmwago@gmail.com>
2019-06-24 07:51:54 +0300

    create new hero form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import spark.ModelAndView;
 
@@ -7,4 +9,19 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.hbs";
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heroes = request.session().attribute("heroes");
+            model.put("heroes", heroes);
+            model.put("templates", "templates/addnewhero.hbs");
+            return new ModelAndView(model, layout);
+        }, new HandlebarsTemplateEngine());
+
+    }
 }
