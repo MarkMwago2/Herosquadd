e3d2ca9761a1941091830008fcd2908c7c36d788
MarkMwago2 <markmwago@gmail.com>
2019-06-25 17:30:37 +0300

    updated port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,23 +6,24 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-//    static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
 
 //        port(getHerokuAssignedPort());
 
-        ProcessBuilder process= new ProcessBuilder();
-        Integer port;
-        if(process.environment().get("PORT")!= null){
-            port = Integer.parseInt(process.environment().get("PORT"));
-        }
+//        ProcessBuilder process= new ProcessBuilder();
+//        Integer port;
+//        if(process.environment().get("PORT")!= null){
+//            port = Integer.parseInt(process.environment().get("PORT"));
+//        }
 
+        port(getHerokuAssignedPort());
 
         staticFileLocation("/public");
 
