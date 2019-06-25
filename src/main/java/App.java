a446511ebe7f932d947474e4c348b91c0cf0a1f2
MarkMwago2 <markmwago@gmail.com>
2019-06-25 16:10:12 +0300

    changed app name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,25 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+//    static int getHerokuAssignedPort() {
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//        if (processBuilder.environment().get("PORT") != null) {
+//            return Integer.parseInt(processBuilder.environment().get("PORT"));
+//        }
+//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+//    }
     public static void main(String[] args) {
-        staticFileLocation("/public");
 
+//        port(getHerokuAssignedPort());
+
+        ProcessBuilder process= new ProcessBuilder();
+        Integer port;
+        if(process.environment().get("PORT")!= null){
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }
+
+
+        staticFileLocation("/public");
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
