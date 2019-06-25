9faf52a7490cfac83833f59cd580770784b55875
MarkMwago2 <markmwago@gmail.com>
2019-06-25 07:46:33 +0300

    create a squad form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 import spark.ModelAndView;
-
 import spark.template.handlebars.HandlebarsTemplateEngine;
-
 import static spark.Spark.*;
-
 public class App {
-
     public static void main(String[] args) {
         staticFileLocation("/public");
-
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
@@ -33,20 +27,38 @@ public class App {
             return new ModelAndView(model,"success.hbs");
         },new HandlebarsTemplateEngine());
 
-        get("/heroes",(request,response)->{
+        get ("/heroes",(request,response)->{
             Map <String,Object> model = new HashMap<String,Object>();
             model.put("heroes",Hero.all());
             model.put("templates","templates/heroes.hbs");
-            return new ModelAndView(model,"heroes.hbs");
+            return new ModelAndView(model,"layout.hbs");
         },new HandlebarsTemplateEngine());
 
-        get("/hero/:id",(request,response)->{
-            Map<String,Object>	model = new HashMap<>();
+        get ("/hero/:id",(request,response)->{
+            Map<String, Object> model = new HashMap<>();
+            String id = request.session().attribute(":id");
+            Hero hero = Hero.find(Integer.parseInt(id));
+
+            // model.put("heroes",request.session().attributes());
+            model.put("hero", hero);
+            model.put("templates", "templates/hero.hbs");
+            return new ModelAndView(model, "layout.hbs");
+        },new HandlebarsTemplateEngine());
+
+        post ("/hero/:id",(request,response)->{
+            Map<String,Object> model = new HashMap<>();
             Hero hero =Hero.find(Integer.parseInt(request.params(":id")));
             model.put("heroes",request.session().attributes());
             model.put("hero",hero);
             model.put("templates","templates/hero.hbs");
-            return new ModelAndView(model,"hero.hbs");
+            return new ModelAndView(model,"heroes.hbs");
         },new HandlebarsTemplateEngine());
+
+        get("/addsquad", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("heroes",Hero.all());
+            model.put("template", "templates/newsquad.vtl");
+            return new ModelAndView(model, "newsquad.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
